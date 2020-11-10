1553579c085fb2822f26d83b8a80f266ec0ea471
Miguel Martín <alu0101209777@ull.edu.es>
2020-11-10 15:07:09 +0000

    Main acepta parámetros por línea de comandos

--- src/main.cpp
+++ src/main.cpp
@@ -4,23 +4,63 @@
 #include <iostream>
 #include <set>
 #include <fstream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
 
 
-int main(void) {
-  int rows = 15;
-  int cols = 15;
+int main(int argc, char* argv[]) {
+
+
+  int rows = 75;
+  int cols = 75;
   int obstacles = 0;
+
+  int a_row, a_col, b_row, b_col;
+
+
+
+  if (argc > 1) {
+    vector<string> opts;
+    
+    for (int i = 1; i < argc; i++)
+      opts.push_back(string(argv[i]));
+
+    string argument;
+    for (int i = 0; i < opts.size();i++) {
+      if (opts[i] == "-r" || opts[i] == "--rows")
+        rows = stoi(opts[++i]);
+      else if (opts[i] == "-c" || opts[i] == "--cols")
+        cols = stoi(opts[++i]);
+      else if (opts[i] == "-o" || opts[i] == "--obstacles")
+        obstacles = stoi(opts[++i]);
+    }
+
+    a_row = 0;
+    a_col = 0;
+    b_row = rows - 1;
+    b_col = cols - 1;
+    
+    Board board(rows,cols);
+    board.set_init_point(a_row,a_col);
+    board.set_final_point(b_row,b_col);
+    board.fill_random_obstacles(obstacles);
+    cout << board << endl << endl;
+    Finder a_star(&board);
+    a_star.find_path(a_row, a_row, b_row, b_col);
+    cout << board << endl;
+
+    return 0;
+  }
+
 
   cout << "\nRows > "; cin >> rows;
   cout << "Cols > "; cin >> cols;
   cout << "Obstacles > "; cin >> obstacles;
   coordinates A, B;
-  int a_row, a_col, b_row, b_col;
-
-  cout << "A coordinates > "; cin >> a_row >> a_col;
+   cout << "A coordinates > "; cin >> a_row >> a_col;
   cout << "B coordinates > "; cin >> b_row >> b_col;
 
   A.row = a_row;
